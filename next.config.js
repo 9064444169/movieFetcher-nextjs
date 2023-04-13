884578d460b551f5ce9eb266fce0806f1a63377d
ankit <ankitxtri@gmail.com>
2023-04-13 12:40:29 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["links.papareact.com", "image.tmdb.org"],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import HeaderItem from "./HeaderItem";
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+const Header = () => {
+  return (
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem Icon={HomeIcon} title="HOME" />
+        <HeaderItem Icon={LightningBoltIcon} title="TRENDING" />
+        <HeaderItem Icon={BadgeCheckIcon} title="VERIFIED" />
+        <HeaderItem Icon={CollectionIcon} title="COLLECTIONS" />
+        <HeaderItem Icon={SearchIcon} title="SEARCH" />
+        <HeaderItem Icon={UserIcon} title="ACCOUNT" />
+      </div>
+      <p className="text-3xl font-[Verdana] text-white text-center">
+        Movie Fetcher
+      </p>
+    </header>
+  );
+};
+
+export default Header;
